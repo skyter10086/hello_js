176299b3973c36b6b316a40e799913b7d8eff10d
skyter10086 <skyter10086@aliyun.com>
2017-08-03 18:05:40 +0800

    update2

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,10 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === '../images/firefox-icon.png') {
-      myImage.setAttribute ('src','../images/firefox2.jpg');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.jpg');
     } else {
-      myImage.setAttribute ('src','../images/firefox-icon.png');
+      myImage.setAttribute ('src','images/firefox-icon.png');
     }
 }
 
